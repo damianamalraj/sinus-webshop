1fac5cf0997cd628c54daf379241d097eaf2b8b1
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-02-26 01:38:14 +0100

    Get order history and order details styling

--- src/views/MyAccountView.vue
+++ src/views/MyAccountView.vue
@@ -1,29 +1,60 @@
 <template>
   <div>
     <div v-if="userInfo">
-    <section class="details-section">
-      <button>EDIT</button>
-      <span>
-      <h2>MY DETAILS</h2>
-      <p>Name: {{userInfo.name}}</p>
-      <p>Address:</p>
-      <p>Street: {{userInfo.address.street}}</p>
-      <p>City: {{userInfo.address.city}}</p>
-      <p>Zip: {{userInfo.address.zip}}</p>
-      </span>
-    </section>
-    <button @click="getAllOrders">Show Order History</button>
-    <section class="orders-section" v-if="showOrders" >
-      <h2>MY ORDERS</h2>
-      <span v-if="allOrders.length">
-      <span v-for="order in allOrders" :key="order.id">
-        <p> {{order.items}} <br> Shipping status:{{order.status}} <br> Shipping Address:{{order.shippingStreet}},<br>{{order.shippingCity}},<br>{{order.shippingZip}}</p>
-      </span>
-      </span>
-      <span v-else>
-        <h3>Currently, there is no order history in your account.</h3>
-      </span>
-    </section>
+      <section class="details-section">
+        <button>EDIT</button>
+        <span>
+          <h2>MY DETAILS</h2>
+          <p>Name: {{ userInfo.name }}</p>
+          <p>Address:</p>
+          <p>Street: {{ userInfo.address.street }}</p>
+          <p>City: {{ userInfo.address.city }}</p>
+          <p>Zip: {{ userInfo.address.zip }}</p>
+        </span>
+      </section>
+      <button @click="getAllOrders">Show Order History</button>
+      <section class="orders-section" v-if="showOrders">
+        <h2>MY ORDERS</h2>
+        <span v-if="allOrders.length">
+          <span v-for="order in allOrders" :key="order.id" class="single-order">
+            <p>
+              Shipping status:{{ order.status }} <br />
+              Shipping Address:{{ order.shippingStreet }},<br />
+              {{ order.shippingCity }},<br />{{ order.shippingZip }}
+            </p>
+            <table class="order-items-list">
+              <tr>
+                <th>Preview</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+              <tr v-for="item in order.items" :key="item.ProductId">
+                <td>
+                  <img
+                    :src="
+                      'http://localhost:5001/images/' +
+                      orderedProduct(item.ProductId).imgFile
+                    "
+                    alt=""
+                  />
+                </td>
+                <td>
+                  Ordered Product: {{ orderedProduct(item.ProductId).title }}
+                  {{ orderedProduct(item.ProductId).category }}
+                  {{ orderedProduct(item.ProductId).shortDesc }}
+                </td>
+                <td>{{ item.amount }}</td>
+                <td>{{ item.price }}</td>
+              </tr>
+            </table>
+            <!-- <li>  Quantity : {{ orderedProduct(item.ProductId) }} <br /> </li> -->
+          </span>
+        </span>
+        <span v-else>
+          <h3>Currently, there is no order history in your account.</h3>
+        </span>
+      </section>
     </div>
     <div v-else>
       <h3>Please login to check your account information and order history</h3>
@@ -33,27 +64,44 @@
 
 <script>
 export default {
-  data(){return{
-    showOrders : false
-  }},
-  computed:{
-    userInfo(){
-      return this.$store.getters.getUserDetails
-    },
-    allOrders(){
-      return this.$store.getters.getOrderHistory
-    }
+  data() {
+    return {
+      showOrders: false,
+    };
   },
-  methods:{
-    getAllOrders(){
-       this.$store.dispatch("fetchAllOrders");
-       this.showOrders = true
-    }
-  }
-   
-}
+  computed: {
+    userInfo() {
+      return this.$store.getters.getUserDetails;
+    },
+    allOrders() {
+      return this.$store.getters.getOrderHistory;
+    },
+    allProducts() {
+      return this.$store.getters.allProducts;
+    },
+  },
+  methods: {
+    orderedProduct(productId) {
+      return this.allProducts.find((item) => item.id == productId);
+    },
+    getAllOrders() {
+      this.$store.dispatch("fetchAllOrders");
+      this.$store.dispatch("getItems");
+      this.showOrders = true;
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped lang="scss">
+img {
+  width: 60px;
+  height: 60px;
+}
+table {
+  place-items: center;
+}
+.order-items-list{
+  margin: auto;
+}
 </style>
